f278d39e0c1309d30ba4c6b32089a352c5ce6455
J0NNYZER0 <jon.ortiz@me.com>
2018-08-07 21:21:58 -0700

    better responsive and home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,13 +4,27 @@ import Footer from '../common/Footer';
 class Home extends React.Component {
   render() {
     return (
-      <main>
+      <main className="home">
         <section>
-        <h1>Full Stack</h1>
-        <h1>Engineer</h1>
-        {/*<div className="layer-one">
-
-        </div>*/}
+          <div>
+            <h1>Full Stack</h1>
+            <h1>Engineer</h1>
+          </div>
+          <div className="tagline">
+            <p className="first">Pushing <b>UI</b>s <b>API</b>s and <b>DB</b>s to the edge of technology.</p>
+            <p>Empowering people to manage, protect and understand data.</p>
+          </div>
+          <div>
+             <a className="down_animation"><span className="down-arrow" /></a>
+          </div>
+        </section>
+        <section>
+          <h1>Full Stack</h1>
+          <h1>Engineer</h1>
+        </section>
+        <section className="last">
+          <h1>Full Stack</h1>
+          <h1>Engineer</h1>
         </section>
         <Footer />
       </main>
